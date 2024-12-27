45e03d736463e68e81263eb144fc4ab9585daaa8
Charles Lee <clee781@gmail.com>
2024-12-27 09:50:35 -0600

    cleanup sandbox test project

--- Test/CallAndResponse.Test.Sandbox/Program.cs
+++ Test/CallAndResponse.Test.Sandbox/Program.cs
@@ -1,37 +1,30 @@
 ﻿#define LOG_TRANSCEIVER_INFO
 using CallAndResponse;
-using CallAndResponse.Transport.Serial;
 using CallAndResponse.Protocol.Modbus;
-using CallAndResponse.Transport.Ble;
-using System.IO.Ports;
-using System.Text;
+using CallAndResponse.Transport.Serial;
 
-var transceiver = new SerialPortTransceiver("COM38", 115200, Parity.None, 8, StopBits.One);
-//var transceiver = new BleNordicUartTransceiver();
-//await transceiver.Open(CancellationToken.None);
+var transceiver = WindowsSerialPortTransceiver.CreateCP210xTransceiver();
+using (var cts = new CancellationTokenSource(5000))
+{
+    try
+    {
+        await transceiver.Open(cts.Token);
+    } catch(Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
 
 var modbusClient = new ModbusRtuClient(transceiver);
-//await modbusClient.Open();
-var dummyData = new byte[230];
-var terminator = new byte[] { 0xda, 0xba, 0xd0, 0x00 };
-dummyData = dummyData.Concat(terminator.ToList()).ToArray();
 while (true)
 {
-    var delayTask = Task.Delay(250);
+    var delayTask = Task.Delay(1000);
     using (var cts = new CancellationTokenSource(250))
     {
         try
         {
-            //var receivedBytes = await transceiver.SendReceive(new byte[] { 0x01, 0x03, 0x00, 0x00, 0x00, 0x01, 0x84, 0x0A }, 15, cts.Token).ConfigureAwait(true);
-            //var responseString = await transceiver.SendReceive("Loopback\n", '\n', cts.Token).ConfigureAwait(true);
-            
-            //var receivedBytes = await transceiver.SendReceive(dummyData, terminator, cts.Token).ConfigureAwait(true);
-            //Console.WriteLine(ToReadableByteArray(receivedBytes.ToArray()));
-
-            var data = await modbusClient.ReadHoldingRegisters(1, 0, numBytes: 96, cts.Token);
-            //Console.WriteLine(ToReadableByteArray(receivedBytes.ToArray()));
-            //Console.WriteLine($"received: {responseString})");
-            Console.WriteLine(Encoding.ASCII.GetString(data.ToArray()));
+            var receivedBytes = await transceiver.SendReceive(new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05 }, 6, cts.Token);
+            Console.WriteLine(ToReadableByteArray(receivedBytes.ToArray()));
         }
         catch (Exception e)
         {
