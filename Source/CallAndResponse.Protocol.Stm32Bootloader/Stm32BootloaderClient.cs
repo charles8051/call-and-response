395b9add96144328e92b4372adee5555637734d5
Charles Lee <clee781@gmail.com>
2025-02-04 01:29:09 -0600

    start ReadMemory method

--- Source/CallAndResponse.Protocol.Stm32Bootloader/Stm32BootloaderClient.cs
+++ Source/CallAndResponse.Protocol.Stm32Bootloader/Stm32BootloaderClient.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using CallAndResponse;
@@ -6,16 +9,12 @@ using CallAndResponse;
 namespace CallAndResponse.Protocol.Stm32Bootloader
 {
 
-    internal enum AckNack{
-        Ack = 0x79,
-        Nack = 0x1F
-    }
     public class Stm32BootloaderClient
     {
         private ITransceiver _transceiver;
 
-        //private static readonly byte Ack = 0x79;
-        //private static readonly byte Nack = 0x1F;
+        private static readonly byte Ack = 0x79;
+        private static readonly byte Nack = 0x1F;
 
         // TODO: Add Transceiver configuration options. The transceiver we use here must be capable of 8 Data Bits, Even Parity, 1 Stop Bit.
         // our BLE implementation won't work out of the box, we'd need a separate BLE Service to configure
@@ -30,20 +29,43 @@ namespace CallAndResponse.Protocol.Stm32Bootloader
             await _transceiver.Open(token);
         }
 
-        public async Task<AckNack> Ping()
+        public async Task<bool> Ping(CancellationToken token = default)
         {
-
+            var result = await _transceiver.SendReceive(new byte[] { 0x7F }, 1, token);
+            if (result.Span[0] == Ack)
+            {
+                return true;
+            }
+            else if (result.Span[0] == Nack)
+            {
+                return false;
+            }
+            else
+            {
+                throw new OperationCanceledException();
+            }
         }
 
-        public async Task<ushort> Get(CancellationToken token = default)
+        public async Task<Stm32ProtocolInfo> GetSupportedCommands(CancellationToken token = default)
         {
-            var result = await _transceiver.SendReceive(new byte[] { (byte)Stm32BootloaderCommand.Get, 0xFF }, 15, token);
-            if (result.Span[0] != (byte)AckNack.Ack)
-            {
-                throw new Exception("Get command failed");
-            }
+            var result = await _transceiver.SendReceive(new byte[] { (byte)Stm32BootloaderCommand.Get, 0xFF }, new byte[] { Ack }, new byte[] { Ack }, token);
 
-            return result.Span[3];
+            var supportedCommands = new List<Stm32BootloaderCommand>(); 
+            foreach (var command in result.Span.Slice(2).ToArray())
+            {
+                if (!Enum.IsDefined(typeof(Stm32BootloaderCommand), command))
+                {
+                    throw new InvalidOperationException($"Unknown command {command}");
+                }
+                supportedCommands.Add((Stm32BootloaderCommand)command);
+            }
+            return new Stm32ProtocolInfo(result.Span[1], supportedCommands);
+        }
+
+        public async Task<byte> Special(CancellationToken token = default)
+        {
+            var result = await _transceiver.SendReceive(new byte[] { (byte)Stm32BootloaderCommand.Special, 0xAF }, 1, token);
+            return result.Span[0];
         }
 
         public async Task GetProtocolVersion(CancellationToken token = default)
@@ -53,19 +75,52 @@ namespace CallAndResponse.Protocol.Stm32Bootloader
 
         public async Task<byte> GetId(CancellationToken token = default)
         {
-            var result = await _transceiver.SendReceive(new byte[] { (byte)Stm32BootloaderCommand.GetId, 0xFD }, 5, token);
-            if (result.Span[0] != (byte)AckNack.Ack)
-            {
-                throw new Exception("Get command failed");
-            }
+            var result = await _transceiver.SendReceive(new byte[] { (byte)Stm32BootloaderCommand.GetId, 0xFD }, new byte[] { Ack }, new byte[] { Ack }, token);
 
-            return result.Span[3];
+            return result.Span[2];
         }
 
-        public async Task<ReadOnlyMemory<byte>> ReadMemory(uint address, ushort length, CancellationToken token = default)
+        public async Task<ReadOnlyMemory<byte>> ReadMemory(uint address, byte length, CancellationToken token = default)
         {
-            throw new NotImplementedException();
+            //            The host sends bytes to the STM32 as follows:
+            //Send ACK byte
+            //Start GID(1)
+            //Received
+            //byte = 0x02 + 0xFD ?
+            //Send N = number of bytes – 1
+            //End of GID(1)
+            //No
+            //Yes
+            //ai14636
+            //Send NACK byte
+            //Send ACK byte
+            //Send product ID
+            //Bytes 1 - 2: 0x11 + 0xEE
+            //Wait for ACK
+            //Bytes 3 to 6 Start address byte 3: MSB, byte 6: LSB
+            //Byte 7: Checksum: XOR(byte 3, byte 4, byte 5, byte 6)
+            //Wait for ACK
+            //Byte 8: The number of bytes to be read – 1(0 < N ≤ 255);
+            //            Byte 9: Checksum: XOR byte 8(complement of byte 8)
+
+
+            // Initiate command
+            await _transceiver.SendReceive(new byte[] { (byte)Stm32BootloaderCommand.ReadMemory, 0xEE }, new byte[] { Ack }, token);
+
+            var addressBytes = BitConverter.GetBytes(address);
+            var checksum = (byte)(addressBytes[0] ^ addressBytes[1] ^ addressBytes[2] ^ addressBytes[3]);
+
+            var sendBytes = addressBytes.ToList();
+            sendBytes.Add(checksum);
+
+            await _transceiver.SendReceive(sendBytes.ToArray(), new byte[] { Ack }, token);
+
+            var byteLengthChecksum = (byte)(length ^ (byte)(~length));
+            await _transceiver.SendReceive( new byte[] { length, byteLengthChecksum }, new byte[] { Ack }, token);
+
+            
         }
+
         public async Task Go(CancellationToken token = default)
         {
             throw new NotImplementedException();
@@ -129,5 +184,23 @@ namespace CallAndResponse.Protocol.Stm32Bootloader
         ReadoutProtect = 0x82,
         ReadoutUnprotect = 0x92,
         GetChecksum = 0xA1
+    }
+
+    public class Stm32ProtocolInfo
+    {
+        public IEnumerable<Stm32BootloaderCommand> SupportedCommands { get; set; }
+        public byte ProtocolVersion { get; set; }
+
+        public Stm32ProtocolInfo(byte protocolVersion, IEnumerable<Stm32BootloaderCommand> supportedCommands)
+        {
+            SupportedCommands = supportedCommands;
+            ProtocolVersion = protocolVersion;
+        }
+
+        public Stm32ProtocolInfo(byte protocolVersion, params Stm32BootloaderCommand[] supportedCommands)
+        {
+            SupportedCommands = supportedCommands;
+            ProtocolVersion = protocolVersion;
+        }
     }
 }
