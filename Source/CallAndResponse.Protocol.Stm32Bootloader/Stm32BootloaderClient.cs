6cfb30fa6ec15f7937cde3fdab98f859607e31e2
Charles Lee <clee781@gmail.com>
2025-02-03 23:53:05 -0600

    implement some commands

--- Source/CallAndResponse.Protocol.Stm32Bootloader/Stm32BootloaderClient.cs
+++ Source/CallAndResponse.Protocol.Stm32Bootloader/Stm32BootloaderClient.cs
@@ -1,87 +1,112 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using CallAndResponse;
 
 namespace CallAndResponse.Protocol.Stm32Bootloader
 {
+
+    internal enum AckNack{
+        Ack = 0x79,
+        Nack = 0x1F
+    }
     public class Stm32BootloaderClient
     {
         private ITransceiver _transceiver;
 
-        private static readonly byte Ack = 0x79;
-        private static readonly byte Nack = 0x1F;
+        //private static readonly byte Ack = 0x79;
+        //private static readonly byte Nack = 0x1F;
 
         // TODO: Add Transceiver configuration options. The transceiver we use here must be capable of 8 Data Bits, Even Parity, 1 Stop Bit.
         // our BLE implementation won't work out of the box, we'd need a separate BLE Service to configure
         public Stm32BootloaderClient(ITransceiver transceiver)
         {
             // 8 Data Bits, Even Parity, 1 Stop Bit
+            _transceiver = transceiver;
         }
 
-        public void Open()
+        public async Task Open(CancellationToken token = default)
+        {
+            await _transceiver.Open(token);
+        }
+
+        public async Task<AckNack> Ping()
         {
 
         }
 
-        public void Get()
+        public async Task<ushort> Get(CancellationToken token = default)
         {
-            throw new NotImplementedException();
+            var result = await _transceiver.SendReceive(new byte[] { (byte)Stm32BootloaderCommand.Get, 0xFF }, 15, token);
+            if (result.Span[0] != (byte)AckNack.Ack)
+            {
+                throw new Exception("Get command failed");
+            }
+
+            return result.Span[3];
         }
 
-        public void GetProtocolVersion()
-        {
-            throw new NotImplementedException();
-        }
-
-        public ushort GetId()
-        {
-            throw new NotImplementedException();
-        }
-
-        public ReadOnlyMemory<byte> ReadMemory(uint address, ushort length)
-        {
-            throw new NotImplementedException();
-        }
-        public void Go()
-        {
-            throw new NotImplementedException();
-        }
-        public void WriteMemory(uint address, ReadOnlyMemory<byte> data)
+        public async Task GetProtocolVersion(CancellationToken token = default)
         {
             throw new NotImplementedException();
         }
 
-        public void EraseMemory(uint address, ushort length)
+        public async Task<byte> GetId(CancellationToken token = default)
+        {
+            var result = await _transceiver.SendReceive(new byte[] { (byte)Stm32BootloaderCommand.GetId, 0xFD }, 5, token);
+            if (result.Span[0] != (byte)AckNack.Ack)
+            {
+                throw new Exception("Get command failed");
+            }
+
+            return result.Span[3];
+        }
+
+        public async Task<ReadOnlyMemory<byte>> ReadMemory(uint address, ushort length, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task Go(CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task WriteMemory(uint address, ReadOnlyMemory<byte> data, CancellationToken token = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task EraseMemory(uint address, ushort length, CancellationToken token = default)
         {
             throw new NotImplementedException();
         }
 
         // Only available for USART Booloader 3.0+
-        public void ExtendedEraseMemory(uint address, ushort length)
+        public async Task ExtendedEraseMemory(uint address, ushort length, CancellationToken token = default)
         {
             throw new NotImplementedException();
         }
 
-        public void WriteProtect()
+        public async Task WriteProtect(CancellationToken token = default)
         {
             throw new NotImplementedException();
         }
 
-        public void WriteUnprotect()
+        public async Task WriteUnprotect(CancellationToken token = default)
         {
             throw new NotImplementedException();
         }
 
-        public void ReadoutProtect()
+        public async Task ReadoutProtect(CancellationToken token = default)
         {
             throw new NotImplementedException();
         }
 
-        public void ReadoutUnprotect()
+        public async Task ReadoutUnprotect(CancellationToken token = default)
         {
             throw new NotImplementedException();
         }
 
-        public void GetChecksum()
+        public async Task GetChecksum(CancellationToken token = default)
         {
             throw new NotImplementedException();
         }
